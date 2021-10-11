49a2c255428d5cbe54aa290da556ce56021d4991
halityldz <83608410+halityldz@users.noreply.github.com>
2021-10-11 15:16:12 +0300

    görev 1
    halit tayyip yıldız

---
+++ CoreDemo/DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LCM0TL4\\SQLEXPRESS;database=CoreBlogDb;integrated security=true;");
+            //conntection string oluşturma 
+
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
